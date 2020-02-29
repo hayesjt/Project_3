89899e2012acf6a8c7778fd42facce3acfb74f33
Haylee Thomas-Kuhlmann <hay.design.lee@gmail.com>
2020-02-29 11:52:31 -0600

    sign up function corrected

--- dashboard/routes/api/users.js
+++ dashboard/routes/api/users.js
@@ -12,6 +12,19 @@ const validateLoginInput = require("../../validation/login");
 // Load User model
 const User = require("../../models/User");
 
+const createUser = (user, hash) => {
+  const newUser = new User({
+    firstname: user.firstname,
+    lastname: user.lastname,
+    email: user.email,
+    password: hash
+  });
+
+  User.create(newUser, function(err, res) {
+    console.log(res);
+  });
+}
+
 // @route POST api/users/register
 router.post("/register", (req, res) => {
   
@@ -27,28 +40,16 @@ router.post("/register", (req, res) => {
     if (user) {
       return res.status(400).json({ email: "Email already exists" });
     } else {
-      const newUser = new User({
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        email: req.body.email,
-        password: req.body.password
-      });
 
-      // Hash password before saving in database
-      bcrypt.genSalt(10, (err, salt) => {
-        bcrypt.hash(newUser.password, salt, (err, hash) => {
-          if (err) throw err;
-          newUser.password = hash;
-          newUser
-            .save()
-            .then(user => res.json(user))
-            .catch(err => console.log(err));
-        });
-      });
+      let password = req.body.password;
 
-      User.create(newUser, function(err, res) {
-        console.log(res);
-      });
+           // Hash password before saving in database
+           bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if (err) throw err;
+              createUser(req.body, hash)
+            });
+          });
 
     }
   });
