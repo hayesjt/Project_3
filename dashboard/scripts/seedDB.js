cc8b5da7b87cce92290217fc9bb4bb2dfdc6153a
Haylee Thomas-Kuhlmann <hay.design.lee@gmail.com>
2020-03-11 19:04:05 -0500

    datas done

--- dashboard/scripts/seedDB.js
+++ dashboard/scripts/seedDB.js
@@ -4,7 +4,7 @@ const db = require("../models");
 // This file empties the Books collection and inserts the books below
 
 mongoose.connect(
-  process.env.MONGODB_URI ||
+  process.env.MONGODB ||
   "mongodb://localhost/Users"
 );
 
